72146c5c0792399d0a8a06145ee47ed185e02b9d
garry426milner <garry426milner@outlook.com>
2025-08-06 13:30:34 +0530

    Create ExpenseList.js

---
+++ src_components/ExpenseList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ExpenseList.css';
+
+function ExpenseList() {
+  return (
+    <div className="expense-list">
+      <h3>Expense List</h3>
+      <ul>
+        <li>Sample Expense - $100</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ExpenseList;
