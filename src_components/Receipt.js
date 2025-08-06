decbaf7e6209ae3a23a96535f9bf98fecfaeff75
garry426milner <garry426milner@outlook.com>
2025-08-06 13:32:12 +0530

    Create Receipt.js

---
+++ src_components/Receipt.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <p>Receipt Component - Details Coming Soon</p>
+    </div>
+  );
+}
+
+export default Receipt;
